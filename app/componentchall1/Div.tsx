dfdceee93bee115ec5cbf95cd85646535041d780
hadil <hadiltech17@gmail.com>
2024-05-27 21:14:25 +0100

    stady map react

--- app/componentchall1/Div.tsx
+++ app/componentchall1/Div.tsx
@@ -1,14 +1,20 @@
+import { title } from "process";
 import Button from "./Button";
 
-const buttonTitles = ["Click me", "Submit", "Cancel" ,"hello ","yahihaidil"];
+const buttonTitles = [
+   { id:1, title: "Click me"},
+   { id:2, title: "Click me" },
+   { id:3, title: "Click me"},
+   { id:4, title: "Click me"},   
+];
 function Div()
 {
     
     return(
         <div className="flex gap-2 border-4 mt-10 p-4 ">
             
-            {buttonTitles.map((title, index) => (
-                <Button key={index} title={title} />
+            {buttonTitles.map((button) => (
+                <Button key={button.id} title={button.title} />
             ))}
 
             
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ import Head from "./componentchall1/Head";
 import Section from "./componentchall1/Section";
 
 import Div from "./componentchall1/Div";
-
+const section= [
+  {id:1,Text:"hello world" ,content:"i'am hadil yahi"},
+  {id:2,Text:"hello" ,content:"i'am Noucha"},
+  {id:3,Text:"hiiiiii" ,content:"i'am khalil"},
+  {id:4,Text:" world" ,content:"i'am chihab"},
+]
 
 export default function Home() {
 
@@ -14,10 +19,11 @@ export default function Home() {
        <Head/>
       <div className="flex justify-evenly">
         <div>
-          <Section Text="hello world" content="i'am hadil yahi"/>
-          <Section Text="Hi noucha" content="hi i'am nocha sister of hadil"/>
-          <Section />
-          <Section/>
+         {section.map((preperis) =>(
+            
+            <Section key={preperis.id} Text={preperis.Text} content={preperis.content}/>
+         ))}
+          
         </div>
         <div>
           <Div/>
