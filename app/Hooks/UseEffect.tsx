d635dc639c093fa9b18ed3074b6b51f72682d1d7
hadil <hadiltech17@gmail.com>
2024-07-03 19:26:00 +0100

    use effect

---
+++ app/Hooks/UseEffect.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+const UseEffect = () => {
+  return (
+  <>
+        {/* useEffect(callback(what to run), dependencies(when to run)) */}
+      
+    
+    </>
+   )
+};
+export default UseEffect;
